f7b21197aac91b3e861ec491f0c30fadb8838091
Mcysneiros <msocys@gmail.com>
2022-01-03 17:29:42 -0300

    :tada:

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 // import styles from '../styles/home.module.scss';
 import Head from 'next/head';
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 
 import { SubscribeButton } from '../components/SubscribeButton';
 import styles from './home.module.scss';
@@ -40,7 +40,7 @@ export default function Home({ product }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1KCOwsBNeDZWsvSVSpwNb7HH', {
     expand: ['product'],
   });
@@ -53,5 +53,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
   };
   return {
     props: { product },
+    revalidate: 60 * 60 * 24, // 24 hours
   };
 };
